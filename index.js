682e26abab2f8e7d132d22fe5560e3ee5febac82
Chanon Onman <chanon@taskworld.com>
2016-09-19 11:21:31 +0700

    add ci

---
+++ index.js
@@ -0,0 +1,35 @@
+const Hapi = require('hapi')
+
+const server = new Hapi.Server()
+server.connection({ port: 13000 })
+
+server.start((err) => {
+  if (err) {
+    throw err
+  }
+  console.log(`Server running at: ${server.info.uri}`)
+})
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply('Hello, world!222')
+  }
+})
+
+server.route({
+  method: 'GET',
+  path: '/echo',
+  handler: function (request, reply) {
+    reply(request.query)
+  }
+})
+
+server.route({
+  method: 'GET',
+  path: '/echo2',
+  handler: function (request, reply) {
+    reply(request.query)
+  }
+})
